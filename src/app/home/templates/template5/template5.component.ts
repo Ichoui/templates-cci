218a30407f87d5979bd092feafce0a554115d721
Morgan Nicol <morganichoui@gmail.com>
2020-10-25 11:35:08 +0100

    oui

---
+++ src/app/home/templates/template5/template5.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cci-template5',
+  templateUrl: './template5.component.html',
+  styleUrls: ['./template5.component.scss']
+})
+export class Template5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
