2ba8695a76fe3952312a0b9217a1049050482a30
Morgan Nicol <morganichoui@gmail.com>
2020-10-25 12:28:54 +0100

    simple basqieu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+export interface TemplateModel {
+  class: string;
+  router: string;
+  title: string;
+}
 
 @Component({
   selector: 'cci-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  template: TemplateModel[] = [
+    { class: 'photo', router: 'photo', title: 'Template Photo' },
+    { class: 'chiffre-cle', router: 'chiffre-cle', title: 'Chiffre-Clé' },
+    { class: 'agenda', router: 'agenda', title: 'Template Agenda' },
+    { class: 'filieres', router: 'filieres', title: 'Template Filières' },
+    { class: 'event', router: 'evenement', title: 'Template Evènement' },
+    { class: 'actu', router: 'actualite', title: 'Template Actualités' },
+  ];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  routage(value) {
+    this.router.navigate([value]);
   }
-
 }
